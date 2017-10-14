7c62350da0a270e979afe01c44267ed674bd2709
Juan <jpmrno@gmail.com>
2017-10-14 14:25:41 -0300

    reformat

--- src/main/java/ar/edu/itba/ss/GranularMediaMain.java
+++ src/main/java/ar/edu/itba/ss/GranularMediaMain.java
@@ -17,7 +17,7 @@ import javafx.geometry.Point2D;
 
 public class GranularMediaMain {
 
-  private static final double BOX_HEIGHT = 10;
+  private static final double BOX_HEIGHT = 2;
   private static final double BOX_WIDTH = 1;
   private static final double BOX_TOP = BOX_HEIGHT * 1.1;
   private static final double BOX_BOTTOM = BOX_TOP - BOX_HEIGHT;
@@ -25,43 +25,44 @@ public class GranularMediaMain {
   private static final double MAX_RADIUS = 0.015;
   private static final double MIN_RADIUS = 0.01;
   private static final double MASS = 0.01;
-  private static final int N = 1;
+  private static final int N = 100;
   private static final double KN = 100000;
   private static final double KT = 2 * KN;
-  private static final double DT = 0.0001;
-  //  private static final double DT = 0.1 * Math.sqrt(MASS / KN);
-  private static final int WRITER_ITERATIONS = (int) (1 / DT) / 10;
+  private static final double DT = 0.00001;
+  //    private static final double DT = 0.1 * Math.sqrt(MASS / KN);
+  private static final int WRITER_ITERATIONS = (int) (1 / DT) / 100;
 
-  public static void main(String[] args) {
-    Particle minParticle = ImmutableParticle.builder()
+  public static void main(final String[] args) {
+    final Particle minParticle = ImmutableParticle.builder()
         .id(1)
         .radius(MIN_RADIUS)
         .mass(MASS)
         .velocity(Point2D.ZERO)
-        .position(new Point2D(0, BOX_BOTTOM))
+        .position(new Point2D(MAX_RADIUS, BOX_BOTTOM))
         .build();
-    Particle maxParticle = ImmutableParticle.builder()
+    final Particle maxParticle = ImmutableParticle.builder()
         .id(N)
         .radius(MAX_RADIUS)
         .mass(MASS)
         .velocity(Point2D.ZERO)
-        .position(new Point2D(BOX_WIDTH, 1.1))
+        .position(new Point2D(BOX_WIDTH - MAX_RADIUS, BOX_TOP))
         .build();
-    List<Particle> initialParticles = RandomParticleGenerator.generateParticles(minParticle,
+    final List<Particle> initialParticles = RandomParticleGenerator.generateParticles(minParticle,
         maxParticle);
 
-    Map<Particle, MovementFunction> functions = new HashMap<>();
-    ContactForceFunction forceFunction = new ContactForceFunction(KN, KT, true);
+    final Map<Particle, MovementFunction> functions = new HashMap<>();
+    final ContactForceFunction forceFunction = new ContactForceFunction(KN, KT, true);
     for (Particle particle : initialParticles) {
       functions.put(particle, new BeemanMovementFunction(forceFunction, Point2D.ZERO));
     }
 
-    GranularMediaSimulator simulator = new GranularMediaSimulator(initialParticles,
+    final GranularMediaSimulator simulator = new GranularMediaSimulator(initialParticles,
         DT, WRITER_ITERATIONS, BOX_WIDTH, BOX_HEIGHT, GAP, functions);
 
-    TimeCriteria timeCriteria = new TimeCriteria(10);
-    ParticlesWriter writer = new BottomGapBoxParticleWriter("simulation_gm",
+    final TimeCriteria timeCriteria = new TimeCriteria(10);
+    final ParticlesWriter writer = new BottomGapBoxParticleWriter("simulation_gm",
         new Point2D(0, BOX_BOTTOM), new Point2D(BOX_WIDTH, BOX_TOP), GAP);
+
     simulator.simulate(timeCriteria, writer);
   }
 }
--- src/main/java/ar/edu/itba/ss/simulator/GranularMediaSimulator.java
+++ src/main/java/ar/edu/itba/ss/simulator/GranularMediaSimulator.java
@@ -7,168 +7,190 @@ import ar.edu.itba.ss.model.ImmutableParticle;
 import ar.edu.itba.ss.model.Neighbour;
 import ar.edu.itba.ss.model.Particle;
 import ar.edu.itba.ss.model.criteria.Criteria;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.ThreadLocalRandom;
 import javafx.geometry.Point2D;
 
-import java.io.IOException;
-import java.util.*;
-import java.util.stream.Collectors;
+public class GranularMediaSimulator implements Simulator {
 
-public class GranularMediaSimulator implements Simulator{
+  private final List<Particle> initialParticles;
+  private final int amountOfParticles;
+  private final double dt;
+  private final int writerIteration;
+  private final double boxWidth;
+  private final double boxHeight;
+  private final double boxTop;
+  private final double boxBottom;
+  private final double gap;
+  private final CellIndexMethod cim;
+  private Map<Particle, MovementFunction> movementFunctions;
 
-    private final List<Particle> initialParticles;
-    private final int amountOfParticles;
-    private final double dt;
-    private final int writerIteration;
-    private final double boxWidth;
-    private final double boxHeight;
-    private final double boxTop;
-    private final double boxBottom;
-    private final double gap;
-    private final CellIndexMethod cim;
-    private Map<Particle, MovementFunction> movementFunctions;
+  public GranularMediaSimulator(List<Particle> initialParticles, double dt, int writerIteration,
+      double boxWidth, double boxHeight, double gap,
+      Map<Particle, MovementFunction> movementFunctions) {
+    this.initialParticles = initialParticles;
+    this.amountOfParticles = initialParticles.size();
+    this.dt = dt;
+    this.writerIteration = writerIteration;
+    this.boxWidth = boxWidth;
+    this.boxHeight = boxHeight;
+    this.boxTop = boxHeight * 1.1;
+    this.boxBottom = boxTop - boxHeight;
+    this.gap = gap;
+    this.cim = new CellIndexMethod(boxTop, false);
+    this.movementFunctions = movementFunctions;
+  }
 
-    public GranularMediaSimulator(List<Particle> initialParticles, double dt, int writerIteration, double boxWidth,
-                                  double boxHeight, double gap,
-                                  Map<Particle, MovementFunction> movementFunctions) {
-        this.initialParticles = initialParticles;
-        this.amountOfParticles = initialParticles.size();
-        this.dt = dt;
-        this.writerIteration = writerIteration;
-        this.boxWidth = boxWidth;
-        this.boxHeight = boxHeight;
-        this.boxTop = boxHeight * 1.1;
-        this.boxBottom = boxTop - boxHeight;
-        this.gap = gap;
-        this.cim = new CellIndexMethod(boxTop, false);
-        this.movementFunctions = movementFunctions;
+  @Override
+  public Set<Particle> simulate(final Criteria endCriteria, final ParticlesWriter writer) {
+    double time = 0;
+    int iteration = 1;
+    List<Particle> particles = initialParticles;
+    double maxRadius = particles.stream().mapToDouble(Particle::radius).max().getAsDouble();
+
+    while (!endCriteria.test(time, particles)) {
+      Map<Particle, Set<Neighbour>> neighbours = cim
+          .apply(particles, maxRadius, 0);
+      particles = nextParticles(neighbours);
+
+      if (iteration == writerIteration) {
+        // TODO: Remove souts
+        System.out.println(
+            "AVG VELOCITY: " + particles.stream().mapToDouble(p -> p.velocity().magnitude())
+                .average().getAsDouble());
+        System.out.println(
+            "MIN POSITION: " + particles.stream().mapToDouble(p -> p.position().getX()).min()
+                .getAsDouble() + ", " + particles.stream().mapToDouble(p -> p.position().getY())
+                .min().getAsDouble());
+        System.out.println(
+            "MAX POSITION: " + particles.stream().mapToDouble(p -> p.position().getX()).max()
+                .getAsDouble() + ", " + particles.stream().mapToDouble(p -> p.position().getY())
+                .max().getAsDouble());
+
+        iteration = 0;
+        try {
+          writer.write(time, neighbours);
+        } catch (IOException e) {
+          e.printStackTrace();
+        }
+      }
+
+      time += dt;
+      iteration++;
     }
 
-    @Override
-    public Set<Particle> simulate(Criteria endCriteria, ParticlesWriter writer) {
-        double time = 0;
-        int iteration = 1;
-        List<Particle> particles = initialParticles;
-        double maxRadius = particles.stream().mapToDouble(Particle::radius).max().getAsDouble();
+    return new HashSet<>(particles);
+  }
 
-        while (!endCriteria.test(time, particles)) {
-            Map<Particle, Set<Neighbour>> neighbours = cim
-                    .apply(particles, maxRadius, 0);
-            particles = nextParticles(neighbours);
+  private List<Particle> nextParticles(final Map<Particle, Set<Neighbour>> neighbours) {
+    final List<Particle> nextParticles = new ArrayList<>(neighbours.size());
 
-            if (iteration == writerIteration) {
-                System.out.println("AVG VELOCITY: " + particles.stream().mapToDouble(p -> p.velocity().magnitude()).average().getAsDouble());
-                System.out.println("MIN POSITION: " + particles.stream().mapToDouble(p -> p.position().getX()).min().getAsDouble() + ", " + particles.stream().mapToDouble(p -> p.position().getY()).min().getAsDouble());
-                System.out.println("MAX POSITION: " + particles.stream().mapToDouble(p -> p.position().getX()).max().getAsDouble() + ", " + particles.stream().mapToDouble(p -> p.position().getY()).max().getAsDouble());
-
-                iteration = 0;
-                try {
-                    writer.write(time, neighbours);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            time += dt;
-            iteration++;
-        }
-
-        return new HashSet<>(particles);
+    for (final Map.Entry<Particle, Set<Neighbour>> entry : neighbours.entrySet()) {
+      nextParticles.add(moveParticle(entry.getKey(), entry.getValue()));
     }
 
-    private List<Particle> nextParticles(Map<Particle, Set<Neighbour>> neighbours) {
-        List<Particle> nextParticles = new ArrayList<>(neighbours.size());
+    return nextParticles;
+  }
 
-        for (Map.Entry<Particle, Set<Neighbour>> entry : neighbours.entrySet()) {
-            nextParticles.add(moveParticle(entry.getKey(), entry.getValue()));
-        }
+  private Particle moveParticle(final Particle particle, final Set<Neighbour> neighbours) {
+    addWallParticles(particle, neighbours);
 
-        return nextParticles;
+    final MovementFunction function = movementFunctions.get(particle);
+    final Particle movedParticle = function.move(particle, neighbours, dt);
+
+    if (movedParticle.position().getY() < 0) {
+      return particleToTop(movedParticle);
     }
 
-    private Particle moveParticle(Particle particle, Set<Neighbour> neighbours) {
-        addWallParticles(particle, neighbours);
+    return movedParticle;
+  }
 
-        MovementFunction function = movementFunctions.get(particle);
-        Particle movedParticle = function.move(particle, neighbours, dt);
-        if(movedParticle.position().getY() < 0){
-            return particleToTop(movedParticle);
-        }
-        return movedParticle;
+  private Particle particleToTop(final Particle particle) {
+    final Point2D newPosition = new Point2D(
+        ThreadLocalRandom.current().nextDouble(particle.radius(), boxWidth - particle.radius()),
+        boxTop
+    );
+
+    final Particle topParticle = ImmutableParticle.builder()
+        .from(particle)
+        .position(newPosition)
+        .velocity(Point2D.ZERO)
+        .build();
+    // TODO: check overlapping
+    movementFunctions.get(particle).clearState(particle);
+//    movementFunctions.put(topParticle, movementFunctions.get(topParticle));
+    return topParticle;
+  }
+
+  private void addWallParticles(final Particle particle, final Set<Neighbour> neighbours) {
+    int wallId = -1;
+
+    // left wall
+    double distanceToWall = particle.position().getX() - particle.radius();
+    if (distanceToWall < 0) {
+      neighbours.add(new Neighbour(ImmutableParticle.builder()
+          .id(wallId--)
+          .position(new Point2D(0, particle.position().getY()))
+          .mass(Double.POSITIVE_INFINITY)
+          .radius(0)
+          .velocity(Point2D.ZERO)
+          .build(), distanceToWall));
     }
 
-    private Particle particleToTop(Particle particle) {
-        Random r = new Random();
-        Particle topParticle = ImmutableParticle.builder()
-                .from(particle)
-                .position(new Point2D(r.nextDouble() * boxWidth,boxTop))
-                .velocity(Point2D.ZERO)
-                .build();
-        //TODO: check overlapping
-        movementFunctions.get(topParticle).clearState(topParticle);
-        movementFunctions.put(topParticle,
-                movementFunctions.get(topParticle));
-        return topParticle;
+    // right wall
+    distanceToWall = boxWidth - (particle.position().getX() + particle.radius());
+    if (distanceToWall < 0) {
+      neighbours.add(new Neighbour(ImmutableParticle.builder()
+          .id(wallId--)
+          .position(new Point2D(boxWidth, particle.position().getY()))
+          .mass(Double.POSITIVE_INFINITY)
+          .radius(0)
+          .velocity(Point2D.ZERO)
+          .build(), distanceToWall));
     }
 
-    private void addWallParticles(Particle particle, Set<Neighbour> neighbours) {
-        int wallId = 0;
-
-        // left wall
-        double distanceToWall = particle.position().getX() - particle.radius();
-        if(distanceToWall < 0){
-            neighbours.add(new Neighbour(ImmutableParticle.builder()
-                    .id(wallId--)
-                    .position(new Point2D(0, particle.position().getY()))
-                    .mass(Double.POSITIVE_INFINITY)
-                    .radius(0)
-                    .velocity(Point2D.ZERO).build(), -distanceToWall));
-        }
-
-        // right wall
-        distanceToWall = boxWidth - (particle.position().getX() + particle.radius());
-        if(distanceToWall < 0){
-            neighbours.add(new Neighbour(ImmutableParticle.builder()
-                    .id(wallId--)
-                    .position(new Point2D(boxWidth, particle.position().getY()))
-                    .mass(Double.POSITIVE_INFINITY)
-                    .radius(0)
-                    .velocity(Point2D.ZERO).build(), -distanceToWall));
-        }
-
-        // down wall
-        double gapStart = boxWidth/2 - gap/2;
-        double gapEnd = boxWidth - gapStart;
-        distanceToWall = particle.position().getY() - particle.radius() - boxBottom;
-        if(distanceToWall < 0 && (particle.position().getX() < gapStart
-                || particle.position().getX() > gapEnd)){
-            neighbours.add(new Neighbour(ImmutableParticle.builder()
-                    .id(wallId--)
-                    .position(new Point2D(particle.position().getX(), boxBottom))
-                    .mass(Double.POSITIVE_INFINITY)
-                    .radius(0)
-                    .velocity(Point2D.ZERO).build(), -distanceToWall));
-        }
-
-        // gap start
-        distanceToWall = gapStart - (particle.position().getX() - particle.radius());
-        if(distanceToWall < 0 && particle.position().getY() == boxBottom){
-            neighbours.add(new Neighbour(ImmutableParticle.builder()
-                    .id(wallId--)
-                    .position(new Point2D(gapStart, 0))
-                    .mass(Double.POSITIVE_INFINITY)
-                    .radius(0)
-                    .velocity(Point2D.ZERO).build(), -distanceToWall));
-        }
-
-        // gap end
-        distanceToWall = gapEnd - (particle.position().getX() + particle.radius());
-        if(distanceToWall < 0 && particle.position().getY() == boxBottom){
-            neighbours.add(new Neighbour(ImmutableParticle.builder()
-                    .id(wallId--)
-                    .position(new Point2D(gapEnd, 0))
-                    .mass(Double.POSITIVE_INFINITY)
-                    .radius(0)
-                    .velocity(Point2D.ZERO).build(), -distanceToWall));
-        }
+    // down wall
+    double gapStart = boxWidth / 2 - gap / 2;
+    double gapEnd = boxWidth - gapStart;
+    distanceToWall = particle.position().getY() - particle.radius() - boxBottom;
+    if (distanceToWall < 0
+        && (particle.position().getX() < gapStart || particle.position().getX() > gapEnd)) {
+      neighbours.add(new Neighbour(ImmutableParticle.builder()
+          .id(wallId--)
+          .position(new Point2D(particle.position().getX(), boxBottom))
+          .mass(Double.POSITIVE_INFINITY)
+          .radius(0)
+          .velocity(Point2D.ZERO)
+          .build(), distanceToWall));
     }
+
+    // gap start
+    distanceToWall = gapStart - (particle.position().getX() - particle.radius());
+    if (distanceToWall < 0 && particle.position().getY() == boxBottom) {
+      neighbours.add(new Neighbour(ImmutableParticle.builder()
+          .id(wallId--)
+          .position(new Point2D(gapStart, 0))
+          .mass(Double.POSITIVE_INFINITY)
+          .radius(0)
+          .velocity(Point2D.ZERO)
+          .build(), distanceToWall));
+    }
+
+    // gap end
+    distanceToWall = gapEnd - (particle.position().getX() + particle.radius());
+    if (distanceToWall < 0 && particle.position().getY() == boxBottom) {
+      neighbours.add(new Neighbour(ImmutableParticle.builder()
+          .id(wallId--)
+          .position(new Point2D(gapEnd, 0))
+          .mass(Double.POSITIVE_INFINITY)
+          .radius(0)
+          .velocity(Point2D.ZERO)
+          .build(), distanceToWall));
+    }
+  }
 }
